99c9584644ea4f42aaac7386db08dfde7f89f903
erai <erai@omiltem.net>
2024-05-26 12:53:11 -0400

    simplify syntax

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,21 @@ GENLEX_SOURCES="genlex.c $LIBS"
 
 ./cc0 $SOURCES -o cc1 || exit 1
 
-./cc1 ${SOURCES} -o cc2 || exit 1
+./cc1 $SOURCES -o cc2 || exit 1
 
 cmp cc1 cc2 || echo mismatch
 
-./cc1 ${GENLEX_SOURCES} -o genlex || exit 1
+./cc1 $GENLEX_SOURCES -o genlex || exit 1
 ./genlex < cc3.l > lex3.c || exit 1
 
-./cc1 ${LIBS} echo.c -o echo || exit 1
-./cc1 ${LIBS} cmp.c -o cmp || exit 1
-./cc1 ${LIBS} rm.c -o rm || exit 1
-./cc1 ${LIBS} ls.c -o ls || exit 1
-./cc1 ${LIBS} cpio.c -o cpio || exit 1
-./cc1 ${LIBS} sh.c -o sh || exit 1
-./cc1 ${LIBS} sshd.c -o sshd || exit 1
-./cc1 ${LIBS} vi.c -o vi || exit 1
+./cc1 $LIBS echo.c -o echo || exit 1
+./cc1 $LIBS cmp.c -o cmp || exit 1
+./cc1 $LIBS rm.c -o rm || exit 1
+./cc1 $LIBS ls.c -o ls || exit 1
+./cc1 $LIBS cpio.c -o cpio || exit 1
+./cc1 $LIBS sh.c -o sh || exit 1
+./cc1 $LIBS sshd.c -o sshd || exit 1
+./cc1 $LIBS vi.c -o vi || exit 1
 
 {
 	echo pxe.asm
